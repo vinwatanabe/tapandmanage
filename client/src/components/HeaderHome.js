7ae3766cf59cf00e9b965536c59ba54ad2b574ea
Vinícius Watanabe <vinicius_watanabe@hotmail.com>
2023-11-14 12:40:09 -0800

    Adjustments on responsive pages

--- client/src/components/HeaderHome.js
+++ client/src/components/HeaderHome.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../images/brand/tap-manage-logo.svg';
 import ArrowIcon from '../images/assets/arrow-ico.svg';
 import ButtonBlue from '../components/ButtonBlue';
 import ButtonPrimary from '../components/ButtonPrimary';
+import { Context } from '../context/AuthContext';
 
 const HeaderHome = () => {
+	const { authenticated } = useContext(Context);
+
+	const userNotLoggedButtons = (
+		<div className='flex flex-row gap-5'>
+			<ButtonBlue
+				text={'Login'}
+				url={'/login'}
+				styles={'border-2 border-white'}
+			/>
+			<ButtonPrimary text={'Register'} url={'/register'} />
+		</div>
+	);
+
+	const userLoggedButtons = (
+		<div className='flex flex-row gap-5'>
+			<ButtonPrimary text={'Console'} url={'/inventory'} />
+		</div>
+	);
+
 	return (
 		<header className='w-full flex flex-row mx-auto bg-darkBlue text-white'>
 			<div className='w-full flex flex-row items-center py-9 sm:gap-0 gap-5 justify-center sm:justify-between mx-auto sm:px-20 2xl:px-0 max-w-7xl flex-wrap sm:flex-nowrap'>
-				<picture>
+				<picture className='flex basis-full sm:basis-auto justify-center'>
 					<img src={Logo} alt='' />
 				</picture>
 
@@ -36,14 +56,7 @@ const HeaderHome = () => {
 					</ul>
 				</div>
 
-				<div className='flex flex-row gap-5'>
-					<ButtonBlue
-						text={'Login'}
-						url={'/login'}
-						styles={'border-2 border-white'}
-					/>
-					<ButtonPrimary text={'Register'} url={'/register'} />
-				</div>
+				<div>{authenticated ? userLoggedButtons : userNotLoggedButtons}</div>
 			</div>
 		</header>
 	);
